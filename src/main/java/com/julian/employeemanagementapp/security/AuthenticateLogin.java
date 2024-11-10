0cb802deece72810cf7d378d1121bd7bc1d34490
Julian Bednarek <juliuszxxbednarek@gmail.com>
2024-11-10 08:37:42 +0100

    removed literal strings and moved them to be constatns

--- src/main/java/com/julian/employeemanagementapp/security/AuthenticateLogin.java
+++ src/main/java/com/julian/employeemanagementapp/security/AuthenticateLogin.java
@@ -9,12 +9,17 @@ import java.sql.SQLException;
 
 public class AuthenticateLogin {
 
+    private static final String DB_ADMIN = "DBAdmins";
+    private static final String DB_USER = "DatabaseUsers";
+    private static final String PASSWORD_COLUMN = "password";
+    private static final String ADMIN_NAME_COLUMN = "AdminName";
+    private static final String USER_LOGIN_COLUMN = "userLogin";
+
     private static boolean authFromDB(LoginData binderBean, String DBTable) {
         String username = binderBean.getUsername();
         String password = binderBean.getPassword();
-        String usernameColumn = DBTable.equals("DBAdmins") ? "AdminName" : "userLogin";
-        String query = "SELECT password FROM " + DBTable + " WHERE " + usernameColumn + " = ?";
-
+        String usernameColumn = DBTable.equals(DB_ADMIN) ? ADMIN_NAME_COLUMN : USER_LOGIN_COLUMN;
+        String query = "SELECT "+ PASSWORD_COLUMN + " FROM " + DBTable + " WHERE " + usernameColumn + " = ?";
 
         try (Connection connection = DBConnector.getInstance().connect();
              PreparedStatement queryStatement = connection.prepareStatement(query)) {
@@ -23,19 +28,16 @@ public class AuthenticateLogin {
             ResultSet resultSet = queryStatement.executeQuery();
 
             if (resultSet.next()) {
-                String dbPassword = resultSet.getString("password");
+                String dbPassword = resultSet.getString(PASSWORD_COLUMN);
                 return PasswordHashing.verifyPassword(password, dbPassword);
             }
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
         return false;
     }
 
-
     public static boolean authenticate(LoginData binderBean) {
-        return authFromDB(binderBean, "DBAdmins") || authFromDB(binderBean, "DatabaseUsers");
+        return authFromDB(binderBean, DB_ADMIN) || authFromDB(binderBean, DB_USER);
     }
 }
